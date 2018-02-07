f1b370c1d60c1eeaf7f61b5c0c2d99cd850cd24e
davmusko <davmusko@gmail.com>
2018-02-07 15:58:27 -0500

    update readwrite and validator

--- src/products/Product.java
+++ src/products/Product.java
@@ -1,52 +1,52 @@
 package products;
 
 public class Product {
-private String name;
-private String category;
-private String description;
-private int price;
+	private String name;
+	private String category;
+	private String description;
+	private int price;
 
-public Product() {
-}
+	public Product() {
+	}
 
-public Product(String name, String category, String description, int price) {
-	super();
-	this.name = name;
-	this.category = category;
-	this.description = description;
-	this.price = price;
-}
+	public Product(String name, String category, String description, int price) {
+		super();
+		this.name = name;
+		this.category = category;
+		this.description = description;
+		this.price = price;
+	}
 
-public String getName() {
-	return name;
-}
+	public String getName() {
+		return name;
+	}
 
-public void setName(String name) {
-	this.name = name;
-}
+	public void setName(String name) {
+		this.name = name;
+	}
 
-public String getCategory() {
-	return category;
-}
+	public String getCategory() {
+		return category;
+	}
 
-public void setCategory(String category) {
-	this.category = category;
-}
+	public void setCategory(String category) {
+		this.category = category;
+	}
 
-public String getDescription() {
-	return description;
-}
+	public String getDescription() {
+		return description;
+	}
 
-public void setDescription(String description) {
-	this.description = description;
-}
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-public int getPrice() {
-	return price;
-}
+	public int getPrice() {
+		return price;
+	}
 
-public void setPrice(int price) {
-	this.price = price;
-}
+	public void setPrice(int price) {
+		this.price = price;
+	}
 
 }
--- src/utilities/ReadWriteFiles.java
+++ src/utilities/ReadWriteFiles.java
@@ -1,5 +1,71 @@
 package utilities;
-
+import java.io.*;
+import java.nio.file.*;
 public class ReadWriteFiles {
 
+
+	public static void createDirectory(String dirString) {
+		Path dirPath = Paths.get(dirString);
+		System.out.println(dirPath.toAbsolutePath());
+		if (Files.notExists(dirPath)) {
+			try {
+				Files.createFile(dirPath);
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				System.out.println("Unknown error. Please contact customer service");
+			}
+		}
+	}
+
+	public static void createFile(String dirString, String fileString) {
+		Path filePath = Paths.get(dirString, fileString);
+		if (Files.notExists(filePath)) {
+			try {
+				Files.createFile(filePath);
+				System.out.println("File created successfully!");
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				System.out.println("Unknown error. Please contact customer service");
+			}
+		}
+
+	}
+	public static void writeToFile(String dirString, String fileString) {
+		Path writeFile = Paths.get(dirString, fileString);
+		
+		File file = writeFile.toFile();
+		
+		try {
+			
+			PrintWriter printOut = new PrintWriter(new FileOutputStream(file, true));
+			printOut.println("Writing to files");//insert what to print in here
+			printOut.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	public static void readFromFile(String dirString, String filePath) {
+		Path readFile = Paths.get(dirString, filePath);
+		
+		File file = readFile.toFile();
+		
+		try {
+			FileReader fr = new FileReader(file);
+			BufferedReader reader = new BufferedReader(fr);
+			String line = reader.readLine();
+			while(line != null) {
+				System.out.println(line);
+				line = reader.readLine();
+			}
+			reader.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("File not found");
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Something went wrong");
+		}
+	}
 }
