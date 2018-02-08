26fabf285212db0f34f88a206fe01695d208dd46
nikkishah-patel <nikkishah.ap@gmail.com>
2018-02-07 20:11:59 -0500

    updated pay class

--- src/pay/Check.java
+++ src/pay/Check.java
@@ -1,5 +1,48 @@
 package pay;
 
 public class Check extends Pay {
+	
+	String customerName;
+	int routingNumber;
+	int checkingNumber;
+	
+	
+	public void Check(String customerName, int routingNumber, int checkingNumber) {
+		this.customerName = customerName;
+		this.routingNumber = routingNumber;
+		this.checkingNumber = checkingNumber;
+	}
+
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+
+	public int getRoutingNumber() {
+		return routingNumber;
+	}
+
+
+	public void setRoutingNumber(int routingNumber) {
+		this.routingNumber = routingNumber;
+	}
+
+
+	public int getCheckingNumber() {
+		return checkingNumber;
+	}
+
+
+	public void setCheckingNumber(int checkingNumber) {
+		this.checkingNumber = checkingNumber;
+	}
+	
 
 }
+
--- src/pay/CreditCard.java
+++ src/pay/CreditCard.java
@@ -1,5 +1,40 @@
 package pay;
 
 public class CreditCard extends Pay {
+	
+	private String customerName;
+	private int cardNumber;
+	private int expDate;
+	private int cvvNumber;
+	
+	public void CreditCard (int cardNumber, int expDate, int cvvNumber) {
+		this.cardNumber = cardNumber;
+		this.expDate = expDate;
+		this.cvvNumber = cvvNumber;
+	}
+
+	public int getCardNumber() {
+		return cardNumber;
+	}
+
+	public void setCardNumber(int cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+
+	public int getExpDate() {
+		return expDate;
+	}
+
+	public void setExpDate(int expDate) {
+		this.expDate = expDate;
+	}
+
+	public int getCvvNumber() {
+		return cvvNumber;
+	}
+
+	public void setCvvNumber(int cvvNumber) {
+		this.cvvNumber = cvvNumber;
+	}
 
 }
